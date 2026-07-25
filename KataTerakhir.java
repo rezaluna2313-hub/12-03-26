98d9d46bd3c71c66053b26d826d5eb083e32d849
Reza Luna 2501091005 <rezaluna2313@gmail.com>
2026-07-25 17:19:51 +0700

    Add files via upload

---
+++ KataTerakhir.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kairanisa_1203;
+
+/**
+ *
+ * @author LAB-SI-PC
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class KataTerakhir {
+    public static void main(String[] args) {
+        BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
+        String word1 = "";
+        String word2 = "";
+        String word3 = "";
+        String hasil = "";
+        
+        try {
+            System.out.print("Enter word1:");
+            word1 = dataIn.readLine();
+            
+            System.out.print("Enter word2:");
+            word2 = dataIn.readLine();
+            
+            System.out.print("Enter word3:");
+            word3 = dataIn.readLine();
+            
+            hasil = word1 + " " + word2 + " " + word3;
+            System.out.println(hasil);
+            
+        } catch (IOException e) {
+            System.out.println("Error!");
+        }
+    }
+}
---
+++ KataTerakhir2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kairanisa_1203;
+
+/**
+ *
+ * @author LAB-SI-PC
+ */
+import javax.swing.JOptionPane;
+
+public class KataTerakhir2 {
+    public static void main(String[] args) {
+        
+        String word1 = JOptionPane.showInputDialog("Enter word1");
+        String word2 = JOptionPane.showInputDialog("Enter word2");
+        String word3 = JOptionPane.showInputDialog("Enter word3");
+        
+        String msg = word1 + " " + word2 + " " + word3;
+        
+        JOptionPane.showMessageDialog(null, msg);
+    }
+}
+
